e2bdd17e6649053aa8e969b97a7a5810235f882a
Chuvaca5256 <Chuvaca5256@users.noreply.github.com>
2026-05-26 20:14:20 -0600

    feat: fondo mosaico Dios Tupa en /organizaciones

--- src/pages/Organizaciones.jsx
+++ src/pages/Organizaciones.jsx
@@ -1,5 +1,29 @@
 import PageStub from './PageStub'
+import DiosTupa from '../assets/mosaicos/Dios_Tupa.png'
 
 export default function Organizaciones() {
-  return <PageStub title="ORGANIZACIONES" />
+  return (
+    <div
+      style={{
+        position: 'relative',
+        minHeight: '100vh',
+      }}
+    >
+      <div
+        style={{
+          position: 'fixed',
+          inset: 0,
+          backgroundImage: `url(${DiosTupa})`,
+          backgroundRepeat: 'repeat',
+          backgroundSize: '80px 80px',
+          opacity: 0.08,
+          pointerEvents: 'none',
+          zIndex: 0,
+        }}
+      />
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <PageStub title="ORGANIZACIONES" />
+      </div>
+    </div>
+  )
 }
